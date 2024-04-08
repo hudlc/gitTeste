7c9a2bf3c64c9d38f6b6207a5d54aa7b8f6f4ac1
Hudson <hudson.95.br@gmail.com>
2024-04-08 10:59:39 -0300

    Adicionado o exemplo do key set

---
+++ KeySet.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+
+public class KeySet {
+
+    public static void main(String[] args) {
+        HashMap<String, Integer> hash = new HashMap<String, Integer>();
+        hash.put("foo", 1);
+        hash.put("bar", 2);
+        hash.put("foobar", 3);
+
+        System.out.println(hash.keySet());
+    }
+
+}
